410cc324b67046f0e32d1d7362b10a3daef4deee
sahil-singh1107 <sahilpython@gmail.com>
2025-02-25 03:45:15 +0530

    added user button

--- src/components/global/sidebar/app-sidebar.tsx
+++ src/components/global/sidebar/app-sidebar.tsx
@@ -1,6 +1,7 @@
 import {Sidebar, SidebarContent, SidebarFooter, SidebarHeader} from "@/components/ui/sidebar";
 import SidebarLogo from "@/components/global/sidebar/sidebar-logo";
 import NavMain from "@/components/global/sidebar/nav-main";
+import {UserButton} from "@clerk/nextjs";
 
 interface AppSidebarProps {
     activeWorkspaceId : string
@@ -9,13 +10,15 @@ interface AppSidebarProps {
 const AppSidebar = ({activeWorkspaceId} : AppSidebarProps) => {
     return <Sidebar collapsible="icon">
         <SidebarHeader className="bg-[#18181a]">
-            <SidebarLogo activeWorkspaceId={activeWorkspaceId} />
+            <SidebarLogo activeWorkspaceId={activeWorkspaceId}/>
         </SidebarHeader>
         <SidebarContent className="bg-[#18181a]">
-            <NavMain activeWorkspaceId={activeWorkspaceId} />
+            <NavMain activeWorkspaceId={activeWorkspaceId}/>
         </SidebarContent>
         <SidebarFooter className="bg-[#18181a]">
-
+            <div className="text-center">
+                <UserButton afterSignOutUrl="/auth/sign-in"/>
+            </div>
         </SidebarFooter>
     </Sidebar>
 }
